d4d4fde8e2e9b3d082cede4ef3d772cd28867af1
root <lawaltemmanuel@gmail>
2022-10-28 13:48:54 -0700

    Added default/dependencies C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char msg[] = "Programming is like building a multilingual puzzle\n";
+char msg[] = "\"Programming is like building a multilingual puzzle\n";
 puts(msg);
 return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - This program displays the size of various C types
+ * depending on the computer it is run
+ * 
+ * Return: (0)
+ */
+
+int main(void)
+{
+char msg1[], msg2[], msg3[];
+msg1[] = "Size of a";
+msg2[] = "Size of an";
+msg
+}
