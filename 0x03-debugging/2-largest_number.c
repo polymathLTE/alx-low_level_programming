7bc579e092890ecfd7595777c6f9568c883c8ff7
root <lawaltemmanuel@gmail>
2022-11-25 07:26:33 -0800

    Added the solutions for task 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int i, largest, size;
+	int lrg[3];
+
+	lrg[0] = a;
+	lrg[1] = b;
+	lrg[2] = c;
+
+	largest = 0;
+	size = sizeof lrg / sizeof *lrg;
+
+	for (i = 0; i < size; i++)
+	{
+		if (lrg[i] > largest)
+		{
+			largest = lrg[i];
+		}
+	}
+	return (largest);
+}
