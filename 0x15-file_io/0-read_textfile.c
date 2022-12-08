d08ab84ba3040160979b1995deda520188379efc
root <lawaltemmanuel@gmail>
2022-12-08 15:12:35 -0800

    Relocated the 'free(buf)' line

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 /*included headers*/
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /*function descriptor*/
 /**
@@ -43,10 +44,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	/*free buf and close processes*/
-	free(buf);
 	close(op);
 
 	/*return*/
 	printf("%s", buf);
+	free(buf);
 	return (wr);
 }
