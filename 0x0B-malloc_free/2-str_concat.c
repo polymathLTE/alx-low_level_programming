6b4fb161d657cb03edbc9f62da35caadcba9b0ca
root <lawaltemmanuel@gmail>
2023-01-18 19:28:34 -0800

    Fixed handling of NULL arguments

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,15 @@ char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
 
-	new_str = malloc((sizeof(char) * (strlen(s1) + strlen(s2))));
+	new_str = malloc((sizeof(char) * (strlen(s1) + strlen(s2) + 1)));
 
 	if (new_str == NULL)
 		return (NULL);
+
+	if (s1 == NULL)
+		strcpy(s1, "");
+	if (s2 == NULL)
+		strcpy(s2, "");
 
 	strcat(new_str, s1);
 	strcat(new_str, s2);
