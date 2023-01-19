af694f18a9e08938545087585128a8b575c7ba5d
root <lawaltemmanuel@gmail>
2023-01-18 19:14:16 -0800

    Fixed issues betty checker warnings filled array with 'c' added checked for malloc error/fail

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,20 +4,27 @@
  * create_array - function that creates an array of chars,
  * and initializes it with a specific char
  * @size: given size of the array
- * @char: given char initializer
+ * @c: given char initializer
  *
  * Return: (*ch_arr) success (NULL) failure
  */
 
 char *create_array(unsigned int size, char c)
 {
+	/*declare variables*/
+	int i;
 	char *ch_arr;
 
 	if (size == 0)
 		return (NULL);
 
-	ch_arr = malloc(size + 1);
-	ch_arr[0] = c;
+	ch_arr = malloc(size);
+
+	if (ch_arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ch_arr[i] = c;
 
 	return (ch_arr);
 }
