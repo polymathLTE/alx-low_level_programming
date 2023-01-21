c674b7340b4cb970c9184fdd704707dee0e5e6b6
root <lawaltemmanuel@gmail>
2023-01-20 19:08:23 -0800

    Added task no -4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,13 +9,11 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
-
 	if (grid == NULL || height == 0)
 		return;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+	for (; height >= 0; height--)
+		free(grid[height]);
 
 	free(grid);
 }
