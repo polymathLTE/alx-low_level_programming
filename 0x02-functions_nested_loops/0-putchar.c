3bb38df8578ab6ebc92430179618abcc4c76d906
polymathLTE <lawaltemmanuel@gmail.com>
2024-01-14 17:30:10 +0100

    Added task file 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - main program
+ *
+ * Description: Write a program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return: (0) - success
+ */
+
+int main(void)
+{
+	char *word = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < strlen(word); i++)
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
