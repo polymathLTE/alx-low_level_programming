64dabe27dfaa0afce33ab7b4572a84d39bebf47a
polymathLTE <lawaltemmanuel@gmail.com>
2024-01-21 16:35:10 +0100

    Modified allocation code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	/*creates an array of specified size*/
-	ar = malloc(sizeof(char) * size);
+	ar = malloc(size);
 
 	if (ar != NULL)
 	{
@@ -26,5 +26,4 @@ char *create_array(unsigned int size, char c)
 	}
 
 	return (NULL);
-	
 }
