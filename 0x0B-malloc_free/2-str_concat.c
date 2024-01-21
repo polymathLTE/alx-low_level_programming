b82fcc27282a6ac487c44e8475941780a69376a9
polymathLTE <lawaltemmanuel@gmail.com>
2024-01-21 17:35:00 +0100

    Added task file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,25 @@
 #include "main.h"
 #include <string.h>
 
-/**
- * str_concat - function that concatenates two strings
- * @s1: given string to be joined - 1
- * @s2: given string to be joined - 2
- *
- * Description: this function concatenates two strings
- * and returns a pointer to the resulting string
- * Return: (new_str) success (NULL) failure/error
- */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *new_str;
+	/*init. variables*/
+	char *concat_str;
+	int i;
 
-	new_str = malloc((sizeof(char) * (strlen(s1) + strlen(s2) + 1)));
+	/*allocate memspace*/
+	concat_str = malloc(sizeof(char) * strlen(s1) + strlen(s2) + 1);
 
-	if (new_str == NULL)
+	/*check for successful allocation*/
+	if (concat_str == NULL)
 		return (NULL);
 
-	if (s1 == NULL)
-		strcpy(s1, "");
-	if (s2 == NULL)
-		strcpy(s2, "");
+	/*write values to memspace*/
+	for (i = 0; i < (int)strlen(s1); i++)
+		concat_str[i] = s1[i];
+	for (i = 0; i < (int)strlen(s2); i++)
+		concat_str[i] = s2[i];
+	concat_str[i] = '\0';
 
-	strcat(new_str, s1);
-	strcat(new_str, s2);
-
-	return (new_str);
-
+	return (concat_str);
 }
