919a292929d1e245144507ffb2dba38addba744d
polymathLTE <lawaltemmanuel@gmail.com>
2024-01-21 17:43:43 +0100

    Modified for acceptinG NULL strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,7 +5,13 @@ char *str_concat(char *s1, char *s2)
 {
 	/*init. variables*/
 	char *concat_str;
-	int i;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+			
 
 	/*allocate memspace*/
 	concat_str = malloc(sizeof(char) * strlen(s1) + strlen(s2) + 1);
@@ -17,8 +23,8 @@ char *str_concat(char *s1, char *s2)
 	/*write values to memspace*/
 	for (i = 0; i < (int)strlen(s1); i++)
 		concat_str[i] = s1[i];
-	for (; i < (int)strlen(s2); i++)
-		concat_str[i] = s2[i];
+	for (j = 0; i < (int)strlen(s2); i++)
+		concat_str[i] = s2[j];
 	concat_str[i] = '\0';
 
 	return (concat_str);
