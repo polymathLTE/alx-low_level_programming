60ddc87c5fcc8894aa4dbfb1a9c2573bb4020342
Lawal Emmanuel <lawaltemmanuel@gmail.com>
2025-08-04 13:08:29 +0100

    Updated the variable type from char

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,18 @@
 
 int main(void)
 {
-char num;
+	int num;
 
-for (num = '0'; num <= '9'; num++)
-{
-putchar(num);
-if (num != '9')
-{
-putchar(',');
-putchar(' ');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-}
-putchar('\n');
-return (0);
-}
+
