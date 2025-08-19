25457dcc69bb8e0385a9acbf33305d12eb51af16
Lawal Emmanuel <lawaltemmanuel@gmail.com>
2025-08-19 14:36:28 +0100

    Added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check the name
+ * @c: the integer to be checked
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && <= 122)
+		_putchar('1' + '0');
+	else
+		_putchar('0' + '0');
+
+}
